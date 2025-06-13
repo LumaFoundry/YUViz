6b51c312508cf422e26ce580f9f462492d5618a7
Jose Manuel Arnaiz Diaz <jarnaiz2002@gmail.com>
2025-06-13 13:59:29 +0100

    Implement texture creation and rendering pipeline

--- src/rendering/videoRenderer.cpp
+++ src/rendering/videoRenderer.cpp
@@ -82,13 +82,90 @@ void VideoRenderer::initialize(QRhi *rhiBackend,
     batch->uploadStaticBuffer(vertexBuffer, vertices);
 
     // Create textures for YUV components
+    // TODO: Check QSize not sure
+    yTex = rhi->newTexture(QRhiTexture::RGBA8, QSize(1, 1), 1, QRhiTexture::RenderTarget | QRhiTexture::Sampled);
+    uTex = rhi->newTexture(QRhiTexture::RGBA8, QSize(1, 1), 1, QRhiTexture::RenderTarget | QRhiTexture::Sampled);
+    vTex = rhi->newTexture(QRhiTexture::RGBA8, QSize(1, 1), 1, QRhiTexture::RenderTarget | QRhiTexture::Sampled);
 
     // Create sampler
+    // TODO: What filters and wrap modes to use? Maybe modularise?
+    sampler = rhi->newSampler(QRhiSampler::Linear, QRhiSampler::Linear, QRhiSampler::ClampToEdge, QRhiSampler::ClampToEdge);
 
     // Create shader resource bindings
+    shaderBindings = rhi->newShaderResourceBindings();
+    shaderBindings->setBindings({
+        QRhiShaderResourceBinding::uniformBuffer(0, QRhiShaderResourceBinding::VertexStage, vertexBuffer),
+        QRhiShaderResourceBinding::sampledTexture(1, QRhiShaderResourceBinding::FragmentStage, yTex, sampler),
+        QRhiShaderResourceBinding::sampledTexture(2, QRhiShaderResourceBinding::FragmentStage, uTex, sampler),
+        QRhiShaderResourceBinding::sampledTexture(3, QRhiShaderResourceBinding::FragmentStage, vTex, sampler)
+    });
 
     // Create graphics pipeline
-    
-
-
+    pipeline = rhi->newGraphicsPipeline();
+    pipeline->setShaderStages({
+        {QRhiShaderStage::Vertex, vertexShaderPath},
+        {QRhiShaderStage::Fragment, fragmentShaderPath}
+    });
+    pipeline->setRenderPassDescriptor(rpDesc);
+    pipeline->setShaderResourceBindings(shaderBindings);
 }
+
+
+void VideoRenderer::uploadFrame(const FrameData& frame) {
+
+    if (!yTex || !uTex || !vTex) {
+        qWarning("Textures not initialized");
+        return;
+    }
+
+    // Upload YUV data to textures
+    QRhiResourceUpdateBatch* batch = rhi->nextResourceUpdateBatch();
+    
+    batch->uploadTexture(yTex, frame.yData, frame.ySize);
+    batch->uploadTexture(uTex, frame.uData, frame.uSize);
+    batch->uploadTexture(vTex, frame.vData, frame.vSize);
+
+    rhi->submitResourceUpdateBatch(batch);
+}
+
+
+void VideoRenderer::render(QRhiCommandBuffer* cb, QRhiRenderPassDescriptor* rp) {
+
+    if (!pipeline || !bindings || !vertexBuffer) {
+        qWarning("Pipeline or bindings not initialized");
+        return;
+    }
+
+    cb->beginPass(rp, Qt::black);
+    
+    cb->setGraphicsPipeline(pipeline);
+    cb->setShaderResources(bindings);
+    cb->setVertexInput(0, vertexBuffer);
+
+    // Draw two triangles forming a rectangle
+    cb->draw(6);
+
+    cb->endPass();
+
+    emit frameRendered();
+}
+
+
+void VideoRenderer::renderFrame(FrameData* frame) {
+    if (!frame) {
+        qWarning("Received null frame");
+        return;
+    }
+
+    uploadFrame(*frame);
+    
+    QRhiCommandBuffer* cb = rhi->currentCommandBuffer();
+    QRhiRenderPassDescriptor* rp = rhi->currentRenderPassDescriptor();
+
+    render(cb, rp);
+
+    // DO NOT delete frame here, it is managed by the caller
+    
+    emit frameRendered();
+}
+
