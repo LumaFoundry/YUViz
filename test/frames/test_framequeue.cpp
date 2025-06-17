83d260c23833b165f93c90303b241a33e09c3da1
Lok Wong <ucabiwo@ucl.ac.uk>
2025-06-17 20:32:04 +0100

    Setup CMakeLists for test and add test_framequeue

---
+++ test/frames/test_framequeue.cpp
@@ -0,0 +1,59 @@
+#include <QtTest>
+#include "frames/frameQueue.h"
+#include "frames/frameMeta.h"
+
+class FrameQueueTest : public QObject {
+    Q_OBJECT
+
+private slots:
+    void testFifoOrder();
+    void testReusability();
+};
+
+void FrameQueueTest::testFifoOrder() {
+    FrameMeta meta;
+    meta.setYWidth(2);
+    meta.setYHeight(2);
+    meta.setUVWidth(1);
+    meta.setUVHeight(1);
+    FrameQueue queue(meta);
+
+    // Write PTS values to 10 frames
+    for (int i = 0; i < 10; ++i) {
+        auto* frame = queue.getTailFrame();
+        frame->setPts(i);
+        queue.incrementTail();
+    }
+
+    // Read back and check order
+    for (int i = 0; i < 10; ++i) {
+        auto* frame = queue.getHeadFrame();
+        QCOMPARE(frame->pts(), int64_t(i));
+        queue.incrementHead();
+    }
+}
+
+void FrameQueueTest::testReusability() {
+    FrameMeta meta;
+    meta.setYWidth(2);
+    meta.setYHeight(2);
+    meta.setUVWidth(1);
+    meta.setUVHeight(1);
+    FrameQueue queue(meta);
+
+    std::set<void*> framePtrs;
+
+    for (int i = 0; i < 100; ++i) {
+        auto* f = queue.getTailFrame();
+        framePtrs.insert(static_cast<void*>(f));
+        queue.incrementTail();
+        queue.getHeadFrame();
+        queue.incrementHead();
+    }
+
+    // Should not exceed queueSize
+    QVERIFY(framePtrs.size() <= 50); 
+}
+
+QTEST_MAIN(FrameQueueTest)
+#include "test_framequeue.moc"
