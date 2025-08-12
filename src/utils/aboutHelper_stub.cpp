7055dadf10cde36aadda2a4e4b9429334dea0b53
Lok Wong <ucabiwo@ucl.ac.uk>
2025-08-12 17:53:58 +0100

    Fix window build failure

--- src/utils/aboutHelper_stub.cpp
+++ src/utils/aboutHelper_stub.cpp
@@ -3,15 +3,22 @@
 #include <QtGui/QGuiApplication>
 #include <QtQml/QQmlEngine>
 #include <QtQml/qqml.h>
-#include "aboutHelper.h"
 
-void AboutHelper::showNativeAbout(const QString&, const QString&, const QString&) {
-    // No-op on non-macOS
+// Dummy QObject for QML singleton registration
+class NativeAboutStub : public QObject {
+  public:
+    explicit NativeAboutStub(QObject* parent = nullptr) :
+        QObject(parent) {}
+    Q_INVOKABLE void showNativeAbout(const QString&, const QString&, const QString&) {
+        // No-op on non-macOS
+    }
+};
+
+static QObject* nativeAbout_stub_singletontype_provider(QQmlEngine*, QJSEngine*) {
+    return new NativeAboutStub();
 }
 
 void registerAboutHelper() {
-    qmlRegisterSingletonType<AboutHelper>(
-        "NativeAbout", 1, 0, "NativeAbout", [](QQmlEngine* engine, QJSEngine*) -> QObject* {
-            return new AboutHelper(engine);
-        });
+    qmlRegisterSingletonType<NativeAboutStub>(
+        "NativeAbout", 1, 0, "NativeAbout", nativeAbout_stub_singletontype_provider);
 }
